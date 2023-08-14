3d1f85a07c242d2d8938956fb50945b3aa1b2a67
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-14 09:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//your JS code here. If required.
+   const fetchButton = document.getElementById('fetch-button');
+        const clickCount = document.getElementById('click-count');
+        const resultsDiv = document.getElementById('results');
+
+        let apiCallCount = 0;
+        let lastApiCallTime = 0;
+        let timerId;
+
+        fetchButton.addEventListener('click', () => {
+            apiCallCount++;
+
+            const currentTime = Date.now();
+            if (currentTime - lastApiCallTime >= 1000) {
+                apiCallCount = 1;
+                lastApiCallTime = currentTime;
+            }
+
+            if (apiCallCount > 5) {
+                alert('Too many API calls. Please wait and try again.');
+                return;
+            }
+
+            lastApiCallTime = currentTime;
+
+            clickCount.textContent = apiCallCount;
+            
+            fetchData()
+                .then(data => {
+                    resultsDiv.textContent = JSON.stringify(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        });
+
+        function fetchData() {
+            return fetch('https://jsonplaceholder.typicode.com/todos/1')
+                .then(response => response.json());
+        }
+
+        function resetClickCount() {
+            apiCallCount = 0;
+            clickCount.textContent = apiCallCount;
+        }
+
+        fetchButton.addEventListener('click', () => {
+            if (timerId) {
+                clearTimeout(timerId);
+            }
+            timerId = setTimeout(resetClickCount, 10000);
+        });
